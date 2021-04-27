18b1ed7bcbbb82db6ab72f5519a491b78f5f6064
kutekumar <kutekumar@gmail.com>
2021-04-27 11:06:20 +0630

    4. Faq Section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,15 @@
 import React from "react"
 import AboutSection from "../components/AboutSection";
-import ServicesSection from "../components/ServicesSection"
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
+
 
 const About = () => {
     return (
         <div>
         <AboutSection />        
         <ServicesSection />
+        <FaqSection />
         </div>
     )
 }
